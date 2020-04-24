52df11ca6b8a500bf68fbce6c946594b8ae3cdba
Tan Doan <tdoan87@gmail.com>
2020-04-24 12:12:26 -0700

    Add initial set of conditions

---
+++ taaap_flush.rb
@@ -0,0 +1,35 @@
+conditions = [
+  "If you were in the Turing blakement (OG nest) (1 sip)",
+  "If you were mentored by a fellow TAAAPER (4 sips)",
+  "You were taught by Jeff for more than 5 classes (3 sips)",
+  "You were in school when Joanne was still working there (2 sips)",
+  "Haven't been to any TAAAP meetup where we ate a bunch of delicious food together (1 sips)",
+  "If you don\"t use VIM (2 sips)",
+  "Pulled an all nighter for any day during your Turing career (2 sips)",
+  "If you were taught by any TAAAP member (2 sips)",
+  "If you've been peer pressured to drink by Tan (4 sips)",
+  "How many TAAAP members in your cohort? (1 sip for each)",
+  "If you've eaten/drank anything new you haven't had before at a TAAAP event (1 sip)",
+  "If you've been mistaken for another TAAAP member (1 sip, 2 extra sips if they're also on the call)",
+  "You don’t get Asian glow when you drink (3 sips)",
+  "Take 2 sips if you didn't repeat a mod",
+  "Take a sip if you are still a student",
+  "Take 2 sips if you haven't started turing yet",
+  "If you have Sriracha in your kitchen right now (2 sips)",
+  "While you were at Turing, you indulged far too much chipottle, or Illegal petes (3 sips)",
+  "You be hackin in VSCODE, take 2 sips",
+  "If you had to actually build furniture and/or clean Turing on the first day of each module. (1 sip)",
+  "If you consistently got free breakfast on Fridays while attending Turing (1 sip)",
+  "If you have bought another Taaap member a drink (4 sips)"
+  "If you’ve presented at a Turing Demo Competition and 5 sips if you’ve placed at a demo comp. 4 sips)",
+  "You’ve been to a TAAAP event where Mike Dao actually showed up (5 sips)",
+  "Take a sip if you're in mod 2 now of Turing",
+  "Take 2 sips if you're in the BE program now",
+  "Take 2 sips if you're in the FE program now",
+  "Take a sip for any mod you got got with a mentor atleast once"
+]
+
+iterator = conditions.shuffle.each
+
+require "pry"; binding.pry;
+
